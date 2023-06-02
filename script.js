2d900df8bf57b1bb0cdf376e4db47c10d0f70a59
Klimbutsky Kostya <pravednik2008@ukr.net>
2023-06-02 17:12:07 +0300

    Add new project

---
+++ script.js
@@ -0,0 +1,13 @@
+function navToggle(){
+	var links = document.getElementById('navUL');
+	links.classList.toggle("_active");
+}
+
+
+$('.owl-carousel').owlCarousel({
+	items:3,
+	lazyLoad:true,
+	loop:true,
+	margin:10
+});
+
